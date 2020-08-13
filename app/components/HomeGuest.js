075da70706b3353fb1aadb07034d673052cdc6db
aman-maharshi <amanm4harshi@gmail.com>
2020-08-13 15:29:47 +0530

    logging in

--- app/components/HomeGuest.js
+++ app/components/HomeGuest.js
@@ -10,10 +10,7 @@ function HomeGuest() {
     async function handleSubmit(e) {
         e.preventDefault()
         try {
-            await Axios.post("http://localhost:8080/register", { username: username, email: email, password: password })
-            setUsername("")
-            setEmail("")
-            setPassword("")
+            await Axios.post("http://localhost:8080/register", { username, email, password })
         } catch (e) {
             console.log(e.response.data)
         }
@@ -32,19 +29,19 @@ function HomeGuest() {
                             <label htmlFor="username-register" className="text-muted mb-1">
                                 <small>Username</small>
                             </label>
-                            <input onChange={e => setUsername(e.target.value)} value={username} id="username-register" name="username" className="form-control" type="text" placeholder="Pick a username" autoComplete="off" />
+                            <input onChange={e => setUsername(e.target.value)} id="username-register" name="username" className="form-control" type="text" placeholder="Pick a username" autoComplete="off" />
                         </div>
                         <div className="form-group">
                             <label htmlFor="email-register" className="text-muted mb-1">
                                 <small>Email</small>
                             </label>
-                            <input onChange={e => setEmail(e.target.value)} value={email} id="email-register" name="email" className="form-control" type="text" placeholder="you@example.com" autoComplete="off" />
+                            <input onChange={e => setEmail(e.target.value)} id="email-register" name="email" className="form-control" type="text" placeholder="you@example.com" autoComplete="off" />
                         </div>
                         <div className="form-group">
                             <label htmlFor="password-register" className="text-muted mb-1">
                                 <small>Password</small>
                             </label>
-                            <input onChange={e => setPassword(e.target.value)} value={password} id="password-register" name="password" className="form-control" type="password" placeholder="Create a password" />
+                            <input onChange={e => setPassword(e.target.value)} id="password-register" name="password" className="form-control" type="password" placeholder="Create a password" />
                         </div>
                         <button type="submit" className="py-3 mt-4 btn btn-lg btn-success btn-block">
                             Sign up for ComplexApp

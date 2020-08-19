1149b2555c30e0d69055e003aaf291d6f641bb31
aman-maharshi <amanm4harshi@gmail.com>
2020-08-19 11:51:59 +0530

    view single post screen added

--- app/components/CreatePost.js
+++ app/components/CreatePost.js
@@ -1,20 +1,21 @@
 import React, { useEffect, useState } from "react"
 import Page from "./Page"
 import Axios from "axios"
+import { withRouter } from "react-router-dom"
 
-function CreatePost() {
+function CreatePost(props) {
     const [title, setTitle] = useState()
     const [body, setBody] = useState()
 
     async function handleSubmit(e) {
-        e.preventDefault();
+        e.preventDefault()
         try {
-            await Axios.post("/create-post", {title, body, token: localStorage.getItem("goSocialToken")})
-        }
-        catch(e) {
+            const response = await Axios.post("/create-post", { title, body, token: localStorage.getItem("goSocialToken") })
+            // redirect to new post url
+            props.history.push(`/post/${response.data}`)
+        } catch (e) {
             console.log(e.response.data)
         }
-
     }
 
     return (
@@ -40,4 +41,4 @@ function CreatePost() {
     )
 }
 
-export default CreatePost
+export default withRouter(CreatePost)
